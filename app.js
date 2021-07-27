54fcdb70392c1a77967da8c7d649fd3b54943665
takanokana <iricocco3@gmail.com>
2021-07-27 13:41:36 +0900

    join message

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const { App } = require('@slack/bolt');
+const { App } = require('@slack/bolt')
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
-});
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+})
 
 // Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
@@ -12,34 +12,46 @@ app.message('hello', async ({ message, say }) => {
   await say({
     blocks: [
       {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Hey there <@${message.user}>!`
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Hey there <@${message.user}>!`,
         },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Click Me"
+        accessory: {
+          type: 'button',
+          text: {
+            type: 'plain_text',
+            text: 'Click Me',
           },
-          "action_id": "button_click"
-        }
-      }
+          action_id: 'button_click',
+        },
+      },
     ],
-    text: `Hey there <@${message.user}>!`
-  });
-});
+    text: `Hey there <@${message.user}>!`,
+  })
+})
 
 app.action('button_click', async ({ body, ack, say }) => {
   // Acknowledge the action
-  await ack();
-  await say(`<@${body.user.id}> clicked the button`);
-});
+  await ack()
+  await say(`<@${body.user.id}> clicked the button`)
+})
 
-(async () => {
+app.event('team_join', async ({ event, client }) => {
+  try {
+    // Call chat.postMessage with the built-in client
+    const result = await client.chat.postMessage({
+      channel: '#room-クリエイティブ-techアンテナ',
+      text: `Welcome to the team, <@${event.user.id}>! 🎉 You can introduce yourself in this channel.`,
+    })
+    console.log(result)
+  } catch (error) {
+    console.error(error)
+  }
+})
+;(async () => {
   // Start your app
-  await app.start(process.env.PORT || 3000);
+  await app.start(process.env.PORT || 3000)
 
-  console.log('⚡️ Bolt app is running!');
-})();
+  console.log('⚡️ Bolt app is running!')
+})()
